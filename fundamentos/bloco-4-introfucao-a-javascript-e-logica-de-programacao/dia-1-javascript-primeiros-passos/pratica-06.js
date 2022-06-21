1de88970410bd5b90e01ee9780ac259aaf649257
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-06-21 20:09:50 -0300

    pratica 6 OR

---
+++ fundamentos/bloco-4-introfucao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/pratica-06.js
@@ -0,0 +1,14 @@
+//Operadores lógicos OR
+let weekDay = "quarta-feira";
+
+if (
+  weekDay == "segunda-feira" ||
+  "terça-feira" ||
+  "quarta-feira" ||
+  "quinta-feira" ||
+  "sexta-feira"
+) {
+  console.log("Oba, mais um dia de aprendizado na Trybe >:D");
+} else {
+  console.log("FINALMENTE, descanso merecido UwU");
+}
