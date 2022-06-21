f7ecd2b25ef355746e80c37125a47079f39fc06c
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-06-21 20:50:38 -0300

    exercicio 2 concluido

--- fundamentos/bloco-4-introfucao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/ex-1.js
+++ fundamentos/bloco-4-introfucao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/ex-1.js
@@ -1,18 +1,18 @@
-const a = 7
-const b = 6
-let message = ''
+const a = 7;
+const b = 6;
+let message = "";
 
 //Adição:
-console.log(message = 'Adição: ' + (a + b));
+console.log((message = "Adição: " + (a + b)));
 
 //Subtração:
-console.log(message = 'Subtração: ' + (a - b));
+console.log((message = "Subtração: " + (a - b)));
 
 //Multiplicação:
-console.log(message = 'Multiplicação: ' + (a * b));
+console.log((message = "Multiplicação: " + a * b));
 
 //Divisão:
-console.log(message = 'Divisão: ' + (a / b));
+console.log((message = "Divisão: " + a / b));
 
 //Módulo:
-console.log(message = 'Módulo: ' + (a % b));
+console.log((message = "Módulo: " + (a % b)));
---
+++ fundamentos/bloco-4-introfucao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/ex-2.js
@@ -0,0 +1,8 @@
+const a = 20
+const b = 28
+
+if (a > b) {
+    console.log(a);
+} else {
+    console.log(b);
+}
