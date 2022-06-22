46cead9f5d0f96664b789eac1e17834914dc6b7e
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-06-22 16:29:06 -0300

    Pratica 2 procurar o indice do valor

--- fundamentos/bloco-4-introfucao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/pratica-01.js
+++ fundamentos/bloco-4-introfucao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/pratica-01.js
@@ -1,6 +1,6 @@
 //Obtenha o valor "Serviços" do array menu:
 
 let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
-let menuServices = menu[1];
+let menuServices = menu.indexOf('Serviços');
 
 console.log(menuServices);
