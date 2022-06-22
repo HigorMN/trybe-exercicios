2a02491ea04f9df1b7a219f6241ff0c5700ae3c4
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-06-21 21:44:13 -0300

    exercicio 5 concluido

--- fundamentos/bloco-4-introfucao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/ex-5.js
+++ fundamentos/bloco-4-introfucao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/ex-5.js
@@ -1,5 +1,3 @@
-// Faça um programa que defina três constantes com os valores dos três ângulos internos de um triângulo. Retorne true se os ângulos representarem os ângulos de um triângulo e false, caso contrário. Se algum ângulo for inválido o programa deve retornar uma mensagem de erro.
-
 const a = 60;
 const b = 90;
 const c = 30;
