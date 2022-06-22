66afad0186c03074cfba616316e79f72322adfdc
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-06-21 21:59:33 -0300

    exercicio 6 concluido

---
+++ fundamentos/bloco-4-introfucao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/ex-6.js
@@ -0,0 +1,26 @@
+let pecas = "rei";
+
+switch (pecas) {
+  case "rei":
+    console.log("O rei pode mover-se em todas as direções");
+    break;
+  case "rainha":
+    console.log("A rainha move-se ao longo da horizontal");
+    break;
+  case "bispo":
+    console.log("O bispo move-se ao longo da diagonal");
+
+  case "cavalo":
+    console.log("É a única peça que pode pular as outras");
+    break;
+  case "torre":
+    console.log("A torre movimenta-se pela vertical ou horizontal");
+    break;
+  case "peão":
+    console.log(
+      "O peão movimenta-se apenas uma casa para frente e somente captura outras peças na diagonal"
+    );
+    break;
+  default:
+    console.log("Erro, nenhuma peça selecionada");
+}
