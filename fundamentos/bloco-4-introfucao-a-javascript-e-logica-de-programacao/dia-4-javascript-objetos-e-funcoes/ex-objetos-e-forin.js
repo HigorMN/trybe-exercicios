fd6b0d937bf06e7c31f1cedacd1aa4baf98b0163
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-06-24 19:09:40 -0300

    exercicios obejetos e for/in 4

--- fundamentos/bloco-4-introfucao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/ex-objetos-e-forin.js
+++ fundamentos/bloco-4-introfucao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/ex-objetos-e-forin.js
@@ -24,3 +24,13 @@ info['recorrente'] = "Sim";
 for(chaves in info){
     console.log(chaves);
 }
+
+// 4 - Faça um novo for/in, mas agora mostre todos os valores das chaves do objeto. Valor esperado no console:
+// Margarida
+// Pato Donald
+// Namorada do personagem principal nos quadrinhos do Pato Donald
+// Sim
+
+for(chaves in info){
+    console.log(info[chaves]);
+}
