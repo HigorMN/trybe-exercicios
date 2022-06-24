f666a8a638b2a4d6870ba7d624c754f16931e06e
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-06-24 11:32:34 -0300

    Primeiro ex concluido

---
+++ fundamentos/bloco-4-introfucao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/ex1.js
@@ -0,0 +1,12 @@
+// 1- O fatorial é a multiplicação de um número natural pelos seus antecessores, exceto o zero. Por exemplo:
+// O fatorial é representado pelo sinal !
+// ! = 4 x 3 x 2 x 1 = 24
+// Com base nessas informações, crie um algoritmo que retorne o fatorial de 10.
+
+let mult = 10;
+
+for (let index = 9; index > 1; index--) {
+    mult = mult * index;
+}
+
+console.log("O fator de 10 é:", mult);
