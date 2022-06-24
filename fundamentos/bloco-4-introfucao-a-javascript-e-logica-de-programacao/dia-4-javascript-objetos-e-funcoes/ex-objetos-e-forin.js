cd0d8989e69797e01fa5b44e2580fc921a797f90
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-06-24 20:26:44 -0300

    exercicios obejetos e for/in 7

--- fundamentos/bloco-4-introfucao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/ex-objetos-e-forin.js
+++ fundamentos/bloco-4-introfucao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/ex-objetos-e-forin.js
@@ -74,3 +74,17 @@ let leitor = {
 //🚀 6 - Acesse as chaves nome, sobrenome e titulo, que está dentro da chave livrosFavoritos, e faça um console.log no seguinte formato: "O livro favorito de Julia Pessoa se chama 'O Pior Dia de Todos'".
 
 console.log("O livro favoito de", leitor.nome, leitor.sobrenome, "se chama " + "'" + leitor.livrosFavoritos[0].titulo + "'");
+
+// 🚀 7 - Adicione um novo livro favorito na chave livrosFavoritos, que é um array. Atribua a esta chave um objeto contendo as seguintes informações:
+
+// {
+//     titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+//     autor: 'JK Rowling',
+//     editora: 'Rocco',
+//   }
+
+leitor.livrosFavoritos.push({
+    titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+    autor: 'JK Rowling',
+    editora: 'Rocco',
+  })
