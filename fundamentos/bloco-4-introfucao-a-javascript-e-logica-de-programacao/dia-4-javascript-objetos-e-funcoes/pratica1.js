43bacf167e20490025992dd86bc3dca912cca8b0
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-06-24 18:28:35 -0300

    praticando requisito 2 concluido

--- fundamentos/bloco-4-introfucao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/pratica1.js
+++ fundamentos/bloco-4-introfucao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/pratica1.js
@@ -9,9 +9,13 @@
 let player = {
     name : 'Marta',
     lastName: 'Silva',
-    age: 24,
+    age: 34,
     medals: {
         golden: 2,
         silver: 3,
     },
 };
+
+// 2 - Acesse as chaves name, lastName e age e concatene as suas informações para imprimir no console uma mensagem no seguinte formato: "A jogadora Marta Silva tem 34 anos de idade".
+
+console.log("A jogadora", player.name, player.lastName, "tem", player.age, "anos de idade.");
