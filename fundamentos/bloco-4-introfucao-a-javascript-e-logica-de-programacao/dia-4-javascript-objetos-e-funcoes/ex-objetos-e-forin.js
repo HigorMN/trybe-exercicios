b1153996368c422a80b20d14a23dcd7a306fcc8f
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-06-24 20:04:02 -0300

    exercicios obejetos e for/in 5

--- fundamentos/bloco-4-introfucao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/ex-objetos-e-forin.js
+++ fundamentos/bloco-4-introfucao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/ex-objetos-e-forin.js
@@ -34,3 +34,39 @@ for(chaves in info){
 for(chaves in info){
     console.log(info[chaves]);
 }
+
+// 5 - Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves. Valor esperado no console:
+
+// Margarida e Tio Patinhas
+// Pato Donald e Christmas on Bear Mountain, Dell's Four Color Comics #178
+// Namorada do personagem principal nos quadrinhos do Pato Donald e O último MacPatinhas
+// Ambos recorrentes // Atenção para essa última linha!
+
+// Usando o objeto abaixo, faça os exercícios a seguir:
+
+// let leitor = {
+//     nome: 'Julia',
+//     sobrenome: 'Pessoa',
+//     idade: 21,
+//     livrosFavoritos: [
+//       {
+//         titulo: 'O Pior Dia de Todos',
+//         autor: 'Daniela Kopsch',
+//         editora: 'Tordesilhas',
+//       },
+//     ],
+//   };
+
+  let info2 = {
+    personagem: "Tio Patinhas",
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: "O último MacPatinhas",
+    recorrente: "Sim",
+  };
+
+  console.log(info.personagem, "e", info2.personagem);
+  console.log(info.origem, "e", info2.origem);
+  console.log(info.nota, "e", info2.nota);
+  if (info.recorrente === info2.recorrente){
+    console.log("Ambos recorrentes");
+  }
