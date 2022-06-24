485ec3354ee26bf9c933b06e77167f4cfc2a3dc6
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-06-24 18:35:51 -0300

    praticando requisito 3 concluido

--- fundamentos/bloco-4-introfucao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/pratica1.js
+++ fundamentos/bloco-4-introfucao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/pratica1.js
@@ -19,3 +19,7 @@ let player = {
 // 2 - Acesse as chaves name, lastName e age e concatene as suas informações para imprimir no console uma mensagem no seguinte formato: "A jogadora Marta Silva tem 34 anos de idade".
 
 console.log("A jogadora", player.name, player.lastName, "tem", player.age, "anos de idade.");
+
+// 3 - Adicione ao objeto a chave bestInTheWorld e atribua a esta chave um array contendo as datas em que a jogadora Marta foi considerada a melhor do mundo.
+
+player['bestInTheWorld'] = [2006, 2007, 2008, 2009, 2010, 2018]
