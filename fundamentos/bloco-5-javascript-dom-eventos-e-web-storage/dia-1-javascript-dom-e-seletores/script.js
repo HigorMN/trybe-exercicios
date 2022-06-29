dd6e53646af65af0e2200a82fd55371ae5de86b9
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-06-28 21:03:47 -0300

    1 exercicio concluido

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -0,0 +1,13 @@
+
+        // Aqui você vai modificar os elementos já existentes utilizando apenas as funções:
+        // - document.getElementById()
+        // - document.getElementsByClassName()
+        // - document.getElementsByTagName()
+        // 1. Crie uma função que mude o texto na tag <p>-----</p> para uma descrição de como você se vê daqui a 2 anos. (Não gaste tempo pensando no texto e sim realizando o exercício)
+        const texto = document.getElementById('paragrafo');
+        texto.innerText = 'O melhor profissional que existe';
+
+        // 2. Crie uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).
+        // 3. Crie uma função que mude a cor do quadrado vermelho para branco.
+        // 4. Crie uma função que corrija o texto da tag <h1>.
+        // 5. Crie uma função que modifique todo o texto da tag <p> para maiúsculo.
