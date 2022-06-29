c1b1eb2a24f368ec663f4b978ce189983d4069f2
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-06-29 18:52:50 -0300

    consertando o ex 5

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/script.js
@@ -1,13 +1,18 @@
+// 1. Acesse o elemento elementoOndeVoceEsta.
 const ex1 = document.querySelector('#elementoOndeVoceEsta')
 
+// 2. Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
 const ex2 = document.querySelector('#elementoOndeVoceEsta').parentElement
 
 ex2.style.color = 'blue'
 
+// 3. Acesse o primeiroFilhoDoFilho e adicione um texto a ele. Você se lembra dos vídeos da aula anterior, como fazer isso?
 const ex3 = document.querySelector('#primeiroFilhoDoFilho').innerText = 'Adicionando um texto';
 
+// 4. Acesse o primeiroFilho a partir de pai.
 const ex4 = document.querySelector('#pai').firstChild
 
+// 5. Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta.
 const ex5 = document.querySelector('#elementoOndeVoceEsta').firstChild
 
 const ex6 = document.querySelector('#elementoOndeVoceEsta').nextSibling
