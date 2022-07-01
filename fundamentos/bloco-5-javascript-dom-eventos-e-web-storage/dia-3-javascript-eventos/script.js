587c18ff5801e941f34135e90a3f147abf01ee4a
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-07-01 13:20:27 -0300

    exercicio 2 concluido

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -22,23 +22,42 @@ function createDaysOfTheWeek() {
 createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
-let decemberDaysList = [
-  29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-  21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
-];
-const diaList = document.getElementById("days");
+// Exercício 1:
+function decemberDays() {
+  let decemberDaysList = [
+    29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
+    20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31,
+  ];
+  const diaList = document.getElementById("days");
 
-for (let diasList = 0; diasList < decemberDaysList.length; diasList += 1) {
-  let days31 = decemberDaysList[diasList];
-  let dias = document.createElement("li");
-  dias.innerText = days31;
-  dias.className = "day";
-  if (days31 === 24 || days31 === 25 || days31 === 31) {
-    dias.classList.add("holiday");
-  }
+  for (let diasList = 0; diasList < decemberDaysList.length; diasList += 1) {
+    let days31 = decemberDaysList[diasList];
+    let dias = document.createElement("li");
+    dias.innerText = days31;
+    dias.className = "day";
+    if (days31 === 24 || days31 === 25 || days31 === 31) {
+      dias.classList.add("holiday");
+    }
 
-  if (days31 === 4 || days31 === 11 || days31 === 18 || days31 === 25) {
-    dias.classList.add("friday");
+    if (days31 === 4 || days31 === 11 || days31 === 18 || days31 === 25) {
+      dias.classList.add("friday");
+    }
+    diaList.appendChild(dias);
   }
-  diaList.appendChild(dias);
 }
+
+decemberDays();
+
+//  Exercício 2:
+function feriados() {
+  const conteinerBotao = document.querySelector(".buttons-container");
+  const paragrafoFeriados = document.createElement("p");
+  const botao = document.createElement("button");
+  botao.id = "btn-holiday";
+  botao.appendChild(paragrafoFeriados).innerText = "Feriado";
+  conteinerBotao.appendChild(botao);
+}
+
+feriados();
+
+// Exercício 3:
