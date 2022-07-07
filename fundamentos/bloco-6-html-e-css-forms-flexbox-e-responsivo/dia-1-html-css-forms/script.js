33e27e3fa1deb99993f10e1d80044c41cb4ef2cb
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-07-06 22:14:18 -0300

    limpar os campos

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -1,7 +1,26 @@
 const button = document.getElementById("button-enviar");
 const nome = document.getElementById("nome");
+const limpe = document.getElementById("limp-btn");
+const email = document.getElementById("email");
+const texto = document.getElementById("texto");
+const radio = document.getElementsByClassName("radio")
 function getName(event) {
-  event.preventDefault();
+//   event.preventDefault();
   console.log(nome.value);
 }
-button.addEventListener('click', getName);
+button.addEventListener("click", getName);
+
+limpe.addEventListener("click", function limpar(event) {
+  event.preventDefault();
+  nome.value = "";
+  email.value = "";
+  texto.value = "";
+});
+
+function radioRemove() {
+    for (let index = 0; index < radio.length; index += 1) {
+        radio[index].value = '';
+    }
+}
+
+radioRemove();
