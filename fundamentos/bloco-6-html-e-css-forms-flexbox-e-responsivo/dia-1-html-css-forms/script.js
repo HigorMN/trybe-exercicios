8e69f83f7a7dbfbbe8bfe79efd5366c2953c60d1
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-07-06 21:57:23 -0300

    Exercicio 2 e 3 tirar o comportamento padrao do button

--- fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -1,10 +1,7 @@
-function buttoncreate() {
-  const forms = document.getElementById("forms");
-  const creatDiv = document.createElement("div");
-  const creatButton = document.createElement("button");
-  creatButton.innerText = "Enviar";
-  forms.appendChild(creatDiv);
-  creatDiv.appendChild(creatButton);
+const button = document.getElementById("button-enviar");
+const nome = document.getElementById("nome");
+function getName(event) {
+  event.preventDefault();
+  console.log(nome.value);
 }
-
-buttoncreate();
+button.addEventListener('click', getName);
