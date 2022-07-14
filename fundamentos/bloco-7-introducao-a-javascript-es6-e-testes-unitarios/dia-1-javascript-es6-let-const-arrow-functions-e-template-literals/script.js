384be53ed8b828fa190445f51358e9c99080e3fb
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-07-14 20:56:10 -0300

    funcao pra ordenar o array em ordem alfabetica

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -1,4 +1,7 @@
-const testingScope = (escopo) =>  (escopo === true) ? console.log('ótimo, fui utilizada no escopo!') : console.log('Não devo ser utilizada fora meu escopo (else)');
+const testingScope = (escopo) =>
+  escopo === true
+    ? console.log("ótimo, fui utilizada no escopo!")
+    : console.log("Não devo ser utilizada fora meu escopo (else)");
 // {
 //     if (escopo === true) {
 //       let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
@@ -8,7 +11,15 @@ const testingScope = (escopo) =>  (escopo === true) ? console.log('ótimo, fui u
 //       let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
 //       console.log(elseScope);
 //     }
-    // console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+// console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
 //   }
 
-  testingScope(true);
+testingScope(true);
+
+const order = (a, b) => a - b
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+oddsAndEvens.sort(order);
+
+
+console.log(oddsAndEvens); // será necessário alterar essa linha 😉
