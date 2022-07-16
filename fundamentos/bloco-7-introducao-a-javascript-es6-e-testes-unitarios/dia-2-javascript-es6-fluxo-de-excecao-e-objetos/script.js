85d82f525282aef3a92429de19718065e88bb28a
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-07-16 17:42:38 -0300

    Mudei nome e total do pagamento e adicionei outra mensagem

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/script.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-2-javascript-es6-fluxo-de-excecao-e-objetos/script.js
@@ -1,49 +1,49 @@
 const order = {
-    name: 'Rafael Andrade',
-    phoneNumber: '11-98763-1416',
-    address: {
-      street: 'Rua das Flores',
-      number: '389',
-      apartment: '701',
-    },
-    order: {
-      pizza: {
-        marguerita: {
-          amount: 1,
-          price: 25,
-        },
-        pepperoni: {
-          amount: 1,
-          price: 20,
-        }
+  name: "Rafael Andrade",
+  phoneNumber: "11-98763-1416",
+  address: {
+    street: "Rua das Flores",
+    number: "389",
+    apartment: "701",
+  },
+  order: {
+    pizza: {
+      marguerita: {
+        amount: 1,
+        price: 25,
       },
-      drinks: {
-        coke: {
-          type: 'Coca-Cola Zero',
-          price: 10,
-          amount: 1,
-        }
+      pepperoni: {
+        amount: 1,
+        price: 20,
       },
-      delivery: {
-        deliveryPerson: 'Ana Silveira',
-        price: 5,
-      }
     },
-    payment: {
-      total: 60,
+    drinks: {
+      coke: {
+        type: "Coca-Cola Zero",
+        price: 10,
+        amount: 1,
+      },
     },
-  };
-  
-  const customerInfo = (order) => {
-    return `Olá ${order.order.delivery.deliveryPerson}, entrega para: ${order.name}, Telefone: ${order.phoneNumber}, R. ${order.address.street}, Nº: ${order.address.number}, AP: ${order.address.apartment}`
-  
-  }
-  
-  console.log(customerInfo(order));
-  
-//   const orderModifier = (order) => {
-//     // Adicione abaixo as informações necessárias.
-  
-//   }
-  
-//   orderModifier(order);
+    delivery: {
+      deliveryPerson: "Ana Silveira",
+      price: 5,
+    },
+  },
+  payment: {
+    total: 60,
+  },
+};
+
+const customerInfo = (order) => {
+  return `Olá ${order.order.delivery.deliveryPerson}, entrega para: ${order.name}, Telefone: ${order.phoneNumber}, R. ${order.address.street}, Nº: ${order.address.number}, AP: ${order.address.apartment}`;
+};
+
+console.log(customerInfo(order));
+
+const orderModifier = () => {
+  order.name = "Luiz Silva";
+  order.payment.total = 50;
+  return `Olá ${order.name}, o total do seu pedido de ${Object.keys(order.order.pizza)} e ${order.order.drinks.coke.type} é R$ ${order.payment.total.toFixed(2)}`
+};
+
+console.log(orderModifier(order));
