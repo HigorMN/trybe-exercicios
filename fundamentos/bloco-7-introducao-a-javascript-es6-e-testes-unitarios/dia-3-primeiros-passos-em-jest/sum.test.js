a6df0570a0ed3afb40b19d04915162cebb8fb01b
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-07-18 19:29:14 -0300

    Exercicio 2.1 concluido

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
@@ -1,13 +1,21 @@
-const sum = require("./sum.js");
+const { sum, myRemove } = require("./sum.js");
 
-test("Teste se o retorno de sum(4, 5) é 9", () => {
-  expect(sum(4, 5)).toBe(9);
+describe("1 - A função sum(a, b) retorna a soma do parâmetro a com o b", () => {
+  test("Teste se o retorno de sum(4, 5) é 9", () => {
+    expect(sum(4, 5)).toBe(9);
+  });
+
+  test("Teste se o retorno de sum(0, 0) é 0", () => {
+    expect(sum(0, 0)).toBe(0);
+  });
+
+  test(`Teste se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")`, () => {
+    expect(() => sum(4, "5")).toThrow("Os parametros não são Numeros");
+  });
 });
 
-test("Teste se o retorno de sum(0, 0) é 0", () => {
-  expect(sum(0, 0)).toBe(0);
-});
-
-test(`Teste se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")`, () => {
-  expect(() => sum(4, "5")).toThrow('Os parametros não são Numeros');
+describe("2 - A função myRemove(arr, item) recebe um array arr e retorna uma cópia desse array sem o elemento item caso ele exista no array", () => {
+  test("Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado", () => {
+    expect(myRemove([1, 2, 3, 4], 3)).toEqual([1, 2, 4]);
+  });
 });
