d73944946b47762438204d8ac13e519579943c26
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-07-18 19:04:35 -0300

    test 4

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.js
@@ -1,3 +1,8 @@
-const sum = (a, b) => a + b;
+const sum = (a, b) => {
+  if (typeof a !== "number" || typeof b !== "number") {
+    throw new Error("Os parametros não são Numeros");
+  }
+  return a + b;
+};
 
 module.exports = sum;
--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
@@ -7,3 +7,7 @@ test("Teste se o retorno de sum(4, 5) é 9", () => {
 test("Teste se o retorno de sum(0, 0) é 0", () => {
   expect(sum(0, 0)).toBe(0);
 });
+
+test(`Teste se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")`, () => {
+  expect(() => sum(4, "5")).toThrow('Os parametros não são Numeros');
+});
