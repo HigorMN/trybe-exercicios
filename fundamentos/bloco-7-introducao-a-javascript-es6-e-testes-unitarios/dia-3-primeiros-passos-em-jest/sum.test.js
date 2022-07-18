d968061809260c877d2ab76ebf95dfa461206185
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-07-18 18:58:44 -0300

    Teste se o retorno de sum(0, 0) é 0

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
@@ -1,5 +1,9 @@
 const sum = require("./sum.js");
 
-test('Teste se o retorno de sum(4, 5) é 9', () => {
-    expect(sum(4, 5)).toBe(9);
-})
+test("Teste se o retorno de sum(4, 5) é 9", () => {
+  expect(sum(4, 5)).toBe(9);
+});
+
+test("Teste se o retorno de sum(0, 0) é 0", () => {
+  expect(sum(0, 0)).toBe(0);
+});
