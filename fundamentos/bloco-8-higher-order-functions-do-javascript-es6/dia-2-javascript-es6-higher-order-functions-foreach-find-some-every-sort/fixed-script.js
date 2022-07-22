f076dffa4ef581540347db316e363eb55838b5af
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-07-22 18:52:27 -0300

    Saber se todos tem a mesma idade e retornar um valor boleano

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/fixed-script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/fixed-script.js
@@ -59,4 +59,20 @@ const hasName = (arr, name) => {
   return arr.some((nome) => nome === name);
 };
 
-console.log(hasName(nomes, "Chico"));
+// console.log(hasName(nomes, "Chico"));
+
+//2 - Escreva uma função que dado um array de pessoas e uma idade mínima retorne true se todas tiverem a idade maior ou igual a mínima e caso contrário false, use every;
+
+const people = [
+  { name: "Mateus", age: 18 },
+  { name: "José", age: 18 },
+  { name: "Ana", age: 23 },
+  { name: "Cláudia", age: 20 },
+  { name: "Bruna", age: 19 },
+];
+
+const verifyAges = (arr, minimumAge) => {
+  return arr.every((idade) => idade.age >= minimumAge);
+};
+
+console.log(verifyAges(people, 18));
