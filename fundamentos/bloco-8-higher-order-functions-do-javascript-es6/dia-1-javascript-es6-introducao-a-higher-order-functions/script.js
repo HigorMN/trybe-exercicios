3aede1bd66de65288ddc6338c75e1523222e70b0
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-07-21 21:19:55 -0300

    contar pontos de uma prova usando CallBack

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-1-javascript-es6-introducao-a-higher-order-functions/script.js
@@ -26,4 +26,28 @@ const numberRandom = (number) => {
 
 const checkHOF = (number, callBack) => callBack(number);
 
-console.log(checkHOF(4, numberRandom));
+// console.log(checkHOF(4, numberRandom));
+
+const RIGHT_ANSWERS = ["A", "C", "B", "D", "A", "A", "D", "A", "D", "C"];
+const STUDENT_ANSWERS = ["A", "N.A", "B", "D", "A", "C", "N.A", "A", "D", "B"];
+
+const test = (rightAnswer, studentAnswers) => {
+  if (rightAnswer === studentAnswers) {
+    return 1;
+  }
+  if (studentAnswers === "N.A") {
+    return 0;
+  }
+  return -0.5;
+};
+
+const counterPoints = (rightAnswer, studentAnswers, action) => {
+  let counter = 0;
+  for (let index = 0; index < rightAnswer.length; index += 1) {
+    const actionReturn = action(rightAnswer[index], studentAnswers[index]);
+    counter += actionReturn;
+  }
+  return `Resultado: ${counter} pontos`
+};
+
+console.log(counterPoints(RIGHT_ANSWERS, STUDENT_ANSWERS, test));
