17c895d7c1650920529dabae5de20d99a760e8fb
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-07-22 16:00:55 -0300

    metodo find para encontrar numeros dentro do arrei que seja divisivel por 3 e 5

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/fixed-script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/fixed-script.js
@@ -12,4 +12,14 @@ const showEmailList = (email) => {
 };
 
 // Adicione seu código aqui
-emailListInData.forEach(showEmailList);
+// emailListInData.forEach(showEmailList);
+
+// 2 - Utilize o find para retornar o primeiro número do array que é divisível por 3 e 5, caso ele exista:
+
+const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+const findDivisibleBy3And5 = () => {
+  return numbers.find((number) => number % 3 === 0 && number % 5 === 0)
+}
+
+console.log(findDivisibleBy3And5())
