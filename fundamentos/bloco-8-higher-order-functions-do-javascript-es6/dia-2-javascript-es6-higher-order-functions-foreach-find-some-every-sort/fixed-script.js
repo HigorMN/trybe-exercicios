491e88791c70979115f10d8946869403b283544f
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-07-22 16:07:27 -0300

    Utilizando o metodo find para encontrar o primeiro nome que é maior que 5 letras

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/fixed-script.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/fixed-script.js
@@ -1,3 +1,4 @@
+// metodo forEach
 // 1 - Use o método forEach chamando a callback showEmailList para apresentar os emails
 
 const emailListInData = [
@@ -14,12 +15,23 @@ const showEmailList = (email) => {
 // Adicione seu código aqui
 // emailListInData.forEach(showEmailList);
 
-// 2 - Utilize o find para retornar o primeiro número do array que é divisível por 3 e 5, caso ele exista:
+// Metodo find();
+// 1 - Utilize o find para retornar o primeiro número do array que é divisível por 3 e 5, caso ele exista:
 
 const numbers = [19, 21, 30, 3, 45, 22, 15];
 
 const findDivisibleBy3And5 = () => {
-  return numbers.find((number) => number % 3 === 0 && number % 5 === 0)
-}
+  return numbers.find((number) => number % 3 === 0 && number % 5 === 0);
+};
 
-console.log(findDivisibleBy3And5())
+// console.log(findDivisibleBy3And5());
+
+// 2 - Utilize o find para encontrar o primeiro nome com cinco letras, caso ele exista:
+
+const names = ["João", "Irene", "Fernando", "Maria"];
+
+const findNameWithFiveLetters = () => {
+  return names.find((nome) => nome.length > 5)
+};
+
+console.log(findNameWithFiveLetters());
