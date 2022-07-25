06412dc8be5f0a929321fc1ef14b07120b4a651f
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-07-25 19:09:24 -0300

    Exercicio 2 concluido

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise1.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise1.js
@@ -68,4 +68,19 @@ function formatedBookNames(arrayBooks) {
   );
 }
 
-console.log(formatedBookNames(books));
+// console.log(formatedBookNames(books));
+
+//🚀 2 - Construa um array de objetos a partir do array de livros. Cada objeto deve conter uma propriedade author, com o nome da pessoa autora do livro, e uma propriedade age com a idade dessa pessoa quando o livro foi lançado. O array deve ser ordenado por idade, ou seja, da pessoa mais jovem para a mais velha considerando suas idades quando o livro foi lançado.
+
+function nameAndAge(arrayBooks) {
+  const names = arrayBooks.map((book) => {
+    const objetos = {};
+    objetos.author = book.author.name;
+    objetos.age = book.releaseYear - book.author.birthYear;
+    return objetos;
+  });
+  names.sort((a, b) => a.age - b.age);
+  return names;
+}
+
+console.log(nameAndAge(books));
