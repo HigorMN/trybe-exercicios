f85062a144e090638bedcc69286c8f1452df4735
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-07-26 19:08:19 -0300

    exercicio 2 concluido

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercise1.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercise1.js
@@ -6,4 +6,86 @@ function flatten(array) {
   return array.reduce((acc, curr) => acc.concat(curr));
 }
 
-console.log(flatten(arrays));
+// console.log(flatten(arrays));
+
+// Para os exercícios 2, 3 e 4 considere o seguinte array:
+
+const books = [
+  {
+    id: 1,
+    name: "As Crônicas de Gelo e Fogo",
+    genre: "Fantasia",
+    author: {
+      name: "George R. R. Martin",
+      birthYear: 1948,
+    },
+    releaseYear: 1991,
+  },
+  {
+    id: 2,
+    name: "O Senhor dos Anéis",
+    genre: "Fantasia",
+    author: {
+      name: "J. R. R. Tolkien",
+      birthYear: 1892,
+    },
+    releaseYear: 1954,
+  },
+  {
+    id: 3,
+    name: "Fundação",
+    genre: "Ficção Científica",
+    author: {
+      name: "Isaac Asimov",
+      birthYear: 1920,
+    },
+    releaseYear: 1951,
+  },
+  {
+    id: 4,
+    name: "Duna",
+    genre: "Ficção Científica",
+    author: {
+      name: "Frank Herbert",
+      birthYear: 1920,
+    },
+    releaseYear: 1965,
+  },
+  {
+    id: 5,
+    name: "A Coisa",
+    genre: "Terror",
+    author: {
+      name: "Stephen King",
+      birthYear: 1947,
+    },
+    releaseYear: 1986,
+  },
+  {
+    id: 6,
+    name: "O Chamado de Cthulhu",
+    genre: "Terror",
+    author: {
+      name: "H. P. Lovecraft",
+      birthYear: 1890,
+    },
+    releaseYear: 1928,
+  },
+];
+
+// Adicione o código do exercício aqui:
+
+// 2 - Crie uma string com os nomes de todas as pessoas autoras.
+
+// const expectedResult = "George R. R. Martin, J. R. R. Tolkien, Isaac Asimov, Frank Herbert, Stephen King, H. P. Lovecraft.";
+
+function reduceNames(arrayToBooks) {
+  return arrayToBooks.reduce((acc, curr, index, array) => {
+    if (index === array.length - 1) {
+      return `${acc} ${curr.author.name}.`;
+    }
+    return `${acc} ${curr.author.name},`;
+  }, '');
+}
+
+console.log(reduceNames(books));
