fad61b9d8713175a79c1beb44e100ea6824bb1f9
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-07-26 23:15:13 -0300

    exercicio 6 concluido

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercise.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercise.js
@@ -143,11 +143,34 @@ const names = [
 ];
 
 function containsA() {
- return names.reduce((acc, curr) => {
-    acc['aMaiuscula'] += curr.split('').filter((a) => a === 'A').length
-    acc['aMinuscula'] += curr.split('').filter((a) => a === 'a').length
-    return acc
-  }, { aMaiuscula: 0, aMinuscula: 0 });
+  return names.reduce(
+    (acc, curr) => {
+      acc["aMaiuscula"] += curr.split("").filter((a) => a === "A").length;
+      acc["aMinuscula"] += curr.split("").filter((a) => a === "a").length;
+      return acc;
+    },
+    { aMaiuscula: 0, aMinuscula: 0 }
+  );
 }
 
-console.log(containsA());
+// console.log(containsA());
+
+//🚀 6.**- Agora vamos criar um novo array de objetos a partir das informações abaixo, onde cada objeto terá o formato { name: nome do aluno, average: media das notas }. Para isso vamos assumir que a posição 0 de notas refere-se ao aluno na posição 0 de alunos, aqui além de reduce será necessário utilizar também a função map. Dica: Você pode acessar o index do array dentro de map, e você pode ver o objeto esperado na constante expected.
+
+const students = ["Pedro Henrique", "Miguel", "Maria Clara"];
+const grades = [
+  [9, 8, 10, 7, 5],
+  [10, 9, 9, 10, 8],
+  [10, 7, 10, 8, 9],
+];
+
+function studentAverage() {
+  return students.map((estudante, index) => {
+    return {
+      name: estudante,
+      average: (grades[index].reduce((acc, curr) => acc + curr, 0) / grades[index].length),
+    };
+  });
+}
+
+console.log(studentAverage());
