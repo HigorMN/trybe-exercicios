67cd9648e7d5e36433401594bf1b53feed8998df
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-07-28 12:46:02 -0300

    exercicio 2 concluido

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise.js
@@ -10,5 +10,14 @@ const rectangle3 = [6, 9];
 const rectangles = [rectangle1, rectangle2, rectangle3];
 
 rectangles.forEach((rectangle) => {
-  console.log(rectangleArea(...rectangle));
+//   console.log(rectangleArea(...rectangle));
 });
+
+// 2 - Crie uma função sum que dado um número variável de elementos retorna a soma desses elementos.
+
+//Dica: use parâmetro rest.
+
+const sum = (...rectangle) =>
+  rectangle.reduce((acumulador, current) => acumulador + current, 0);
+
+console.log(sum(2, 3, 4, 5, 98));
