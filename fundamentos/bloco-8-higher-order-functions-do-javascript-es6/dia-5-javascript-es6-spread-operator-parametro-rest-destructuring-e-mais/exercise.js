675b8fbea2663d3db6c571fd54142a4f4054b89f
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-07-28 12:59:09 -0300

    exercicio 3 concluido

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise.js
@@ -10,7 +10,7 @@ const rectangle3 = [6, 9];
 const rectangles = [rectangle1, rectangle2, rectangle3];
 
 rectangles.forEach((rectangle) => {
-//   console.log(rectangleArea(...rectangle));
+  //   console.log(rectangleArea(...rectangle));
 });
 
 // 2 - Crie uma função sum que dado um número variável de elementos retorna a soma desses elementos.
@@ -20,4 +20,28 @@ rectangles.forEach((rectangle) => {
 const sum = (...rectangle) =>
   rectangle.reduce((acumulador, current) => acumulador + current, 0);
 
-console.log(sum(2, 3, 4, 5, 98));
+// console.log(sum(2, 3, 4, 5, 98));
+
+//3 - Escreva a função personLikes, que dado um objeto de parâmetro que representa uma pessoa, retorna todos os gostos daquela pessoa, conforme mostrado abaixo:
+
+// Dica: use object destructuring.
+
+const alex = {
+    name: 'Alex',
+    age: 26,
+    likes: ['fly fishing'],
+    nationality: 'Australian',
+  };
+  
+  const gunnar = {
+    name: 'Gunnar',
+    age: 30,
+    likes: ['hiking', 'scuba diving', 'taking pictures'],
+    nationality: 'Icelandic',
+  };
+  
+  // complete a assinatura da função abaixo
+  const personLikes = ({name, age, likes}) => `${name} is ${age} years old and likes ${likes.join(', ')}.`;
+  
+  console.log(personLikes(alex)); // 'Alex is 26 years old and likes fly fishing.'
+  console.log(personLikes(gunnar)); // 'Gunnar is 30 years old and likes hiking, scuba diving, taking pictures.'
