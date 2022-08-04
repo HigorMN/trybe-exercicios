9dbc8e2694366c3cc0ed5a31c10832844e7ac3d6
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-08-04 15:28:17 -0300

    exercicio de fixação 1

--- front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/tests.js
+++ front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/tests.js
@@ -6,3 +6,4 @@ setTimeout(() => {
   console.log('1 - Receber roda'); // 1 - Receber roda
   console.log('2 - Encaixar parafusos'); // 2 - Encaixar parafusos
   console.log('3 - Fixar roda no carro'); // 3 - Fixar roda no carro
+  
