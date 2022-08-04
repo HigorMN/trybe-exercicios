c387b92e3bd841843107490866d444d8544b8813
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-08-04 15:57:03 -0300

    exercicio de fixação 1 organizando

--- front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/fixation-exercise.js
+++ front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/fixation-exercise.js
@@ -18,53 +18,29 @@
 
 // console.log(numbers); // O retorno é [2, 3]
 
-// const pushNumber = (list, number) => list.push(number);
+const pushNumber = (list, number) => list.push(number);
 
-// let numbers = [];
+let numbers = [];
 
-// setTimeout(() => pushNumber(numbers, 1), 3000);
-// pushNumber(numbers, 2);
-// pushNumber(numbers, 3);
+setTimeout(() => pushNumber(numbers, 1), 3000);
+pushNumber(numbers, 2);
+pushNumber(numbers, 3);
 
-// setTimeout(() => console.log(numbers), 3000);
+setTimeout(() => console.log(numbers), 3000);
 
 // Definição da função userFullName
-// const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
-
-// // Definição da função getUser
-// const getUser = (param) => {
-//   const userToReturn = {
-//     firstName: "Ivan",
-//     lastName: "Ivanovich",
-//     nationality: "Russian"
-//   };
-//   // Retornamos nosso parâmetro, que será uma função (callback)
-//   return param(userToReturn);
-// };
-
-// // Chamada/execução da função getUser, que vai receber como parâmetro nossa função userFullName.
-// console.log(getUser(userFullName));
-
-// 1 - Adicione uma callback como parâmetro da função getUser.
-
-// No código abaixo você tem a função getUser, que define um objeto com os dados de uma pessoa. Complete a função getUser de forma que ela receba uma função callback como parâmetro para realizar as operações abaixo:
-
-// Insira o retorno da função getUser;
-// Complete a chamada da função getUser de modo que o retorno seja: "Hello! My name is Ivan Ivanovich";
-// Complete a chamada da função getUser de modo que o retorno seja: "Ivan is Russian".
-
 const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
-const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
 
-const getUser = (callback) => {
+// Definição da função getUser
+const getUser = (param) => {
   const userToReturn = {
     firstName: "Ivan",
     lastName: "Ivanovich",
     nationality: "Russian"
   };
-  return callback(userToReturn)
-  // Insira o retorno da função `getUser`
+  // Retornamos nosso parâmetro, que será uma função (callback)
+  return param(userToReturn);
 };
 
-console.log(getUser(userFullName)); // complete a chamada da função getUser de modo que o retorno seja: "Hello! My name is Ivan Ivanovich"
-console.log(getUser(userNationality)); // complete a chamada da função getUser de modo que o retorno seja: "Ivan is Russian"
+// Chamada/execução da função getUser, que vai receber como parâmetro nossa função userFullName.
+console.log(getUser(userFullName));
