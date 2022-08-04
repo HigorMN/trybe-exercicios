47f6d4fe58a08d8f7e8183f749cbd701bea25305
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-08-04 15:48:27 -0300

    FullName com callback

--- front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/fixation-exercise.js
+++ front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/fixation-exercise.js
@@ -27,3 +27,20 @@
 // pushNumber(numbers, 3);
 
 // setTimeout(() => console.log(numbers), 3000);
+
+// Definição da função userFullName
+const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+
+// Definição da função getUser
+const getUser = (param) => {
+  const userToReturn = {
+    firstName: "Ivan",
+    lastName: "Ivanovich",
+    nationality: "Russian"
+  };
+  // Retornamos nosso parâmetro, que será uma função (callback)
+  return param(userToReturn);
+};
+
+// Chamada/execução da função getUser, que vai receber como parâmetro nossa função userFullName.
+console.log(getUser(userFullName));
