affb7af25267abbf492489b123a91f1dabdda29d
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-08-16 15:59:09 -0300

    fixacão concluida

---
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/fixar-aprendizado/src/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+class About extends React.Component {
+  render() {
+    const skills = ["HTML",'CSS', 'JAVASCRIPT', 'REACT']; // adicione mais habilidades aqui
+    const jsxSkills = skills.map((skill) => <li>{skill}</li>); // cria um array de tags <li>
+    return (
+      <div>
+        <h1>Higor Maranhão</h1>
+        <p>Sou Fullstacks developer</p>
+
+        <h2>Minhas Habilidades</h2>
+        <ul>{jsxSkills}</ul>
+      </div>
+    );
+  }
+}
+
+export default About;
---
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/fixar-aprendizado/src/App.js
@@ -0,0 +1,9 @@
+// import logo from "./logo.svg";
+import "./App.css";
+import About from "./About";
+
+function App() {
+  return <About />;
+}
+
+export default App;
