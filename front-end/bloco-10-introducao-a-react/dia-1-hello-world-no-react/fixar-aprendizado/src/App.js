a80c7b39c430cc475dc1895dbe43975737096d38
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-08-16 16:22:26 -0300

    Exercicio concluido

--- front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/fixar-aprendizado/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-1-hello-world-no-react/fixar-aprendizado/src/App.js
@@ -2,8 +2,17 @@
 import "./App.css";
 import About from "./About";
 
+const Task = (value) => {
+  return (
+    <li>{value}</li>
+  );
+}
+
 function App() {
-  return <About />;
+  return (
+    <About />,
+    Task('Higor')
+  );
 }
 
 export default App;
