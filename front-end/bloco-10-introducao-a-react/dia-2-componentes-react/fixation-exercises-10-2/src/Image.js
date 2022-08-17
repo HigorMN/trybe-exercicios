53444623135a68a250b63fb1985913c9eb83eea4
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-08-17 12:59:29 -0300

    Imagem pedida adicionada

--- front-end/bloco-10-introducao-a-react/dia-2-componentes-react/fixation-exercises-10-2/src/Image.js
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/fixation-exercises-10-2/src/Image.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 class Image extends React.Component {
   render() {
-    return <img width={500} src={'https://veja.abril.com.br/wp-content/uploads/2019/12/amazonia-floresta-coraccca7ao.jpg.jpg'} alt={'Coração na floresta'} />;
+    return <img src={'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg'} alt={'Cute cat staring'} />;
   }
 }
 
