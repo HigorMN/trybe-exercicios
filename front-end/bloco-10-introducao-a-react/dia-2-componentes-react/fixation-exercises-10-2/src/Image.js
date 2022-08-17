ad347f2241af99ee1f23010ffae35c2afe99c8d9
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-08-17 12:58:12 -0300

    imagem adicionada e testada

--- front-end/bloco-10-introducao-a-react/dia-2-componentes-react/fixation-exercises-10-2/src/Image.js
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/fixation-exercises-10-2/src/Image.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 class Image extends React.Component {
   render() {
-    return <img src={this.props.source} alt={this.props.alternativeText} />;
+    return <img width={500} src={'https://veja.abril.com.br/wp-content/uploads/2019/12/amazonia-floresta-coraccca7ao.jpg.jpg'} alt={'Coração na floresta'} />;
   }
 }
 
