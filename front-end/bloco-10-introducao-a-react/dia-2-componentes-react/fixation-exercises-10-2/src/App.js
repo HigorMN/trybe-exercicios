49eb2fb409b4d6e3e65c027e6f9f1c97dd4f4342
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-08-17 14:56:53 -0300

    fixando props

--- front-end/bloco-10-introducao-a-react/dia-2-componentes-react/fixation-exercises-10-2/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/fixation-exercises-10-2/src/App.js
@@ -1,11 +1,34 @@
 // import logo from './logo.svg';
 import './App.css';
-import Image from './Image';
+// import Image from './Image';
+import Order from './Order';
 
 function App() {
+  const headphone = {
+    id: 102,
+    user: 'cena@gmail.com',
+    product: 'Razer Headphone',
+    price: {
+      value: 99.99,
+      currency: 'dollars',
+    },
+  };
+
+  const energyDrink = {
+    id: 77,
+    user: 'cena@gmail.com',
+    product: 'Monster 500mL',
+    price: {
+      value: 9.99,
+      currency: 'dollars',
+    },
+  };
+
   return (
-    <div>
-      <Image />
+    <div className='App'>
+      <h1> Orders recently created </h1>
+      <Order order={headphone} />
+      <Order order={energyDrink} />
     </div>
   );
 }
