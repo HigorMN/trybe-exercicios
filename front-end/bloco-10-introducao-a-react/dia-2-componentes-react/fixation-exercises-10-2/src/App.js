98a4253824f9e2187afc24ddd9e0069c6bf128de
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-08-17 15:47:23 -0300

    fixando exercicio 3

--- front-end/bloco-10-introducao-a-react/dia-2-componentes-react/fixation-exercises-10-2/src/App.js
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/fixation-exercises-10-2/src/App.js
@@ -1,34 +1,28 @@
 // import logo from './logo.svg';
 import './App.css';
 // import Image from './Image';
-import Order from './Order';
+// import Order from './Order';
+import UserProfile from './UserProfile';
 
 function App() {
-  const headphone = {
-    id: 102,
-    user: 'cena@gmail.com',
-    product: 'Razer Headphone',
-    price: {
-      value: 99.99,
-      currency: 'dollars',
+  const users = [
+    {
+      id: 102,
+      name: "João",
+      email: "joao@gmail.com",
+      avatar: "https://cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_640.png"
     },
-  };
-
-  const energyDrink = {
-    id: 77,
-    user: 'cena@gmail.com',
-    product: 'Monster 500mL',
-    price: {
-      value: 9.99,
-      currency: 'dollars',
-    },
-  };
+    {
+      id: 77,
+      name: "Amélia",
+      email: "amelia@gmail.com",
+      avatar: "https://cdn.pixabay.com/photo/2017/01/31/13/05/cameo-2023867_640.png"
+    }
+  ];
 
   return (
-    <div className='App'>
-      <h1> Orders recently created </h1>
-      <Order order={headphone} />
-      <Order order={energyDrink} />
+    <div className="App">
+      {users.map(user => <UserProfile user={user} />)}
     </div>
   );
 }
--- front-end/bloco-10-introducao-a-react/dia-2-componentes-react/fixation-exercises-10-2/src/Image.js
+++ front-end/bloco-10-introducao-a-react/dia-2-componentes-react/fixation-exercises-10-2/src/Image.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 class Image extends React.Component {
   render() {
-    return <img src={'https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg'} alt={'Cute cat staring'} />;
+    return <img src={this.props.source} alt={this.props.alternativeText} />;
   }
 }
 
