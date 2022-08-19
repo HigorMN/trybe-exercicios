278c7227fbf59484e176d008bfb5acab5f3f714f
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-08-19 15:51:16 -0300

    Numero de clickes com estado

--- front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
+++ front-end/bloco-11-componentes-com-estado-eventos-e-formularios-com-react/dia-1-componentes-com-estado-e-eventos/fancy-buttons/src/App.js
@@ -2,12 +2,35 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
-  handleClick = () => {
-    console.log('Clicou!')
+  constructor() {
+    super();
+    // Removemos a declaração do estado de dentro do construtor
+    // this.state = {
+    //   numeroDeCliques: 0,
+    // };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  // Fazemos a definição do estado utilizando a sintaxe Public Class Field
+  state = {
+    numeroDeCliques: 0,
+  };
+
+  handleClick() {
+    this.setState((estadoAnterior, _props) => ({
+      numeroDeCliques: estadoAnterior.numeroDeCliques + 1,
+    }));
   }
 
   render() {
-    return <button onClick={this.handleClick}>Meu botão</button>
+    const { numeroDeCliques } = this.state;
+    return (
+      <div>
+        <button type="button" onClick={ this.handleClick }>Meu Botão</button>
+        <p>Numero de cliques: { numeroDeCliques }</p>
+      </div>
+    );
   }
 }
 
