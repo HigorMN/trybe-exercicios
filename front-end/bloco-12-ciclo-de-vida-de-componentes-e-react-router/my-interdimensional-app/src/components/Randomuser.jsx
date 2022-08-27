a64902e1fb47694772f0de4adce56c1328876739
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-08-26 21:12:16 -0300

    aparecer loading

--- front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/my-interdimensional-app/src/components/Randomuser.jsx
+++ front-end/bloco-12-ciclo-de-vida-de-componentes-e-react-router/my-interdimensional-app/src/components/Randomuser.jsx
@@ -1,9 +1,27 @@
 import React, { Component } from 'react'
 
 export default class Randomuser extends Component {
+  state = {
+    loading: true,
+    data: [],
+  }
+
+  async componentDidMount(){
+   const API = await fetch('https://api.randomuser.me/');
+   const json = await API.json();
+   this.setState({
+    loading: false,
+    data: json.results
+  })
+  }
+
   render() {
+    const { loading, data } = this.state;
+    const load = <p>Carregando...</p>
     return (
-      <div>Randomuser</div>
+      <div>
+        {loading ? load : data.map()}
+      </div>
     )
   }
 }
