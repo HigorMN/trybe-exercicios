6b2395e492f7bc2e901ad43baced757b916dcc90
Higor Maranhão <higor.maranhao2000@gmail.com>
2022-09-14 21:04:21 -0300

    final do dia

--- front-end/bloco-14-testes-automatizados-com-react-testing-library/testes-react/src/App.test.js
+++ front-end/bloco-14-testes-automatizados-com-react-testing-library/testes-react/src/App.test.js
@@ -1,8 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Verifica se existe um input email na tela', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const inputEmail = screen.getByLabelText(/email/i);
+  expect(inputEmail).toBeInTheDocument();
+  expect(inputEmail.type).toBe("email")
 });
+
+test('verifica se existe um botão', () => {
+  render(<App />)
+})
+
+//acesar os elementos da tela
