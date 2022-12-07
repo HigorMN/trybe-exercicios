97147f179891f84455f32b337f92ffa9077c9995
HigorMN <higor.maranhao2000@gmail.com>
2022-12-07 13:25:13 -0300

    feat(exercise6C): funcao que remove o id 6 e 10 do simpsonsJSON

--- back-end/section-4-introfucao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-runtime-assincono/my-scripts/simpsons.js
+++ back-end/section-4-introfucao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-runtime-assincono/my-scripts/simpsons.js
@@ -8,20 +8,29 @@ async function getSimpsonById(id) {
   if (!chosenSimpson) {
     throw new Error('id não encontrado');
   }
-  
+
   return chosenSimpson;
 }
 
+async function filterSimpsons() {
+  const fileContent = await fs.readFile('./simpsons.json', 'utf-8');
+  const simpsons = JSON.parse(fileContent);
+  const newArray = simpsons.filter((simpson) => simpson.id !== '10' && simpson.id !== '6');
+
+  await fs.writeFile('./simpsons.json', JSON.stringify(newArray));
+}
+
 async function readAll() {
-  const fileContent = await fs.readFile('./simpsons.json');
+  const fileContent = await fs.readFile('./simpsons.json', 'utf-8');
   const simpsons = JSON.parse(fileContent);
   const strings = simpsons.map(({ id, name }) => `${id} - ${name}`);
   strings.forEach((string) => console.log(string));
 }
 
 async function main() {
+  filterSimpsons();
   // await readAll();
-  console.log(await getSimpsonById(1));
+  // console.log(await getSimpsonById(1));
 }
 
 main();
