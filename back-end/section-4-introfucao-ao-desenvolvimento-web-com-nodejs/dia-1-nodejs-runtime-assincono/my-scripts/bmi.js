ee3fcb5f6f82f6b99f604c25c623bb26af12ea09
HigorMN <higor.maranhao2000@gmail.com>
2022-12-07 12:44:47 -0300

    feat(exercise6A): funcao que imprime todos os personagem dos simpsons no console

--- back-end/section-4-introfucao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-runtime-assincono/my-scripts/bmi.js
+++ back-end/section-4-introfucao-ao-desenvolvimento-web-com-nodejs/dia-1-nodejs-runtime-assincono/my-scripts/bmi.js
@@ -12,6 +12,12 @@ const BMI = (peso, altura) => {
 const main = () => {
   const bmi = BMI(weight, height);
   console.log(`BMI: ${bmi}`);
+  if(bmi < 18.5) console.log('Abaixo do peso (magreza)');
+  if(bmi >= 18.5 && bmi <= 24.9) console.log('Peso normal');
+  if(bmi >= 25 &&bmi <= 29.9) console.log('Acima do peso (sobrepeso)');
+  if(bmi >= 30 && bmi <= 34.9) console.log('Obesidade grau I');
+  if(bmi >= 35 && bmi <= 39.9) console.log('Obesidade grau II');
+  if(bmi > 40) console.log('Obesidade graus III e IV');
 }
 
 main();
