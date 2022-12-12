33a57a1e2d7d97182e3ba2e01e3695d0dd282ff2
HigorMN <higor.maranhao2000@gmail.com>
2022-12-12 20:48:24 -0300

    feat(exercise 5): created middlewares validatePrice

--- back-end/section-4-introfucao-ao-desenvolvimento-web-com-nodejs/dia-4-node-js-express-e-middlewares/src/app.js
+++ back-end/section-4-introfucao-ao-desenvolvimento-web-com-nodejs/dia-4-node-js-express-e-middlewares/src/app.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const validateName = require('./middlewares/validateName');
+const vilidatePrice = require('./middlewares/validatePrice');
 
 const app = express();
 
 app.use(express.json());
 
-app.post('/activities', validateName, (_req, res) => {
+app.post('/activities', validateName, vilidatePrice, (_req, res) => {
   res.status(201).json({ "message": "Atividade cadastrada com sucesso!" })
 });
 
