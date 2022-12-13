3563aa2a21c9b261a87c903a8c6ede9b93f9fce4
HigorMN <higor.maranhao2000@gmail.com>
2022-12-12 21:21:26 -0300

    feat(exercise 8): created middlewares validade Rating

--- back-end/section-4-introfucao-ao-desenvolvimento-web-com-nodejs/dia-4-node-js-express-e-middlewares/src/app.js
+++ back-end/section-4-introfucao-ao-desenvolvimento-web-com-nodejs/dia-4-node-js-express-e-middlewares/src/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const { validateCreatedAt } = require('./middlewares/validateCreatedAt');
 const { validateDescription } = require('./middlewares/validateDescription');
+const { validateRating } = require('./middlewares/validateRating');
 const validateName = require('./middlewares/validateName');
 const vilidatePrice = require('./middlewares/validatePrice');
 
@@ -7,8 +9,16 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/activities', validateName, vilidatePrice, validateDescription, (_req, res) => {
-  res.status(201).json({ "message": "Atividade cadastrada com sucesso!" })
-});
+app.post(
+  '/activities',
+  validateName,
+  vilidatePrice,
+  validateDescription,
+  validateCreatedAt,
+  validateRating,
+  (_req, res) => {
+    res.status(201).json({ message: 'Atividade cadastrada com sucesso!' });
+  }
+);
 
 module.exports = app;
---
+++ back-end/section-4-introfucao-ao-desenvolvimento-web-com-nodejs/dia-4-node-js-express-e-middlewares/src/middlewares/validateRating.js
@@ -0,0 +1,15 @@
+const validateRating = (req, res, next) => {
+  const { rating } = req.body.rating;
+
+  if (!Number.isInteger(rating) || rating < 1 || rating > 5) {
+    return res
+      .status(400)
+      .json({ message: 'O campo rating deve ser um número inteiro entre 1 e 5' });
+  }
+
+  next();
+};
+
+module.exports = {
+  validateRating,
+};
