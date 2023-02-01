d2b0c35ef0cf41e9cd37b8124727b64915d5e87f
Higor Maranhão <higor.maranhao2000@gmail.com>
2023-02-01 16:14:02 -0300

    feat: calculando area

---
+++ back-end/section-8-introdução-ao-typescript/exercises/dist/exercises.js
@@ -0,0 +1,24 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.triangleCheck = exports.getPolygonPerimeter = exports.getTriangleArea = exports.getRectangleArea = exports.getSquareArea = void 0;
+const getSquareArea = (side) => side ** 2;
+exports.getSquareArea = getSquareArea;
+const getRectangleArea = (base, height) => {
+    return base * height;
+};
+exports.getRectangleArea = getRectangleArea;
+const getTriangleArea = (base, height) => {
+    return (base * height) / 2;
+};
+exports.getTriangleArea = getTriangleArea;
+function getPolygonPerimeter(sides) {
+    return sides.reduce((acc, side) => acc + side, 0);
+}
+exports.getPolygonPerimeter = getPolygonPerimeter;
+function triangleCheck(sideA, sideB, sideC) {
+    const checkSideA = (sideB - sideC) < sideA && sideA < (sideB + sideC);
+    const checkSideB = (sideA - sideC) < sideB && sideB < (sideA + sideC);
+    const checkSideC = (sideA - sideB) < sideC && sideC < (sideA + sideB);
+    return checkSideA && checkSideB && checkSideC;
+}
+exports.triangleCheck = triangleCheck;
